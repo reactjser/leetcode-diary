7e7d42134f601d836a9d80935cec7777e15e748f
liuyao <reactjser@163.com>
2019-10-12 14:23:09 +0800

    add 3

---
+++ src/3无重复字符的最长子串.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let freq = new Array(128).fill(0);
+  let l = 0,
+    r = -1; // 滑动窗口为s[l...r]
+  let res = 0;
+
+  while (l < s.length - 1) {
+    if (r + 1 < s.length && freq[s[r + 1].charCodeAt(0)] === 0) {
+      freq[s[++r].charCodeAt(0)]++;
+    } else {
+      freq[s[l++].charCodeAt(0)]--;
+    }
+
+    res = Math.max(res, r - l + 1);
+  }
+
+  return res;
+};
+
+const s = 'abcabcbb';
+const ret = lengthOfLongestSubstring(s);
+console.log(ret);
